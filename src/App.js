47fae7464ce03df7e0eddbcf8c96bbed52785ee4
harihardhikcs127 <harihardhik.221cs127@nitk.edu.in>
2024-11-01 04:32:13 +0530

    Speakers Judges Done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Landing from "./components/Landing";
 import About from "./components/About";
 import "./index.css";
 import StatsSection from "./components/StatsSection";
+import { SpeakersJudges } from "./components/SpeakersJudges";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Landing />
       <About />
       <StatsSection />
+      <SpeakersJudges />
       {/* Add other sections like PRIZES, TRACKS, etc. */}
     </div>
   );
