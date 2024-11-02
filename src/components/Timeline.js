27e328b8ef6608e84826e12fdc1d7b43e407c807
harihardhikcs127 <harihardhik.221cs127@nitk.edu.in>
2024-11-02 15:57:47 +0530

    Warnings cleared in build

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Timeline } from "./ui/timeline";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 export function TimelineDemo() {
   const data = [
